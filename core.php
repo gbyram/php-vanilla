ffdd7d3afdcf3bb4f3ce6f5658522d04d6a6b54c
rbrixtel <rbrixtel@c95ee1f9-c27b-13b1-9bc4-0c396e7ae7f5>
2010-07-19 13:47:05 +0000

    première version 0.1

---
+++ core.php
@@ -0,0 +1,26 @@
+<?php
+
+class Core{
+
+    public static function path2fm($_path){
+        $contenu = tool_files::file_load($_path);
+        $document=new document_xml($path,$contenu);
+        $structure=$document->__get_structure();
+        //    $signature = $document->_get_signature();
+        $test=new RecursiveIteratorIterator($structure,RecursiveIteratorIterator::SELF_FIRST);
+        $factory_mot=new factory_mot();
+        foreach($test as $id_bloc=>$bloc_xhtml){
+            $texte=tool_tree::tree_xhtml2texte($bloc_xhtml->__get_value());
+            $texte = tool_string::clean_guill($texte);
+            $texte = tool_string::clean_non_end($texte);
+            $texte = tool_string::clean_digit($texte);
+            $texte = tool_string::exception_lg($texte,$ligne['lg']);
+            $texte = tool_string::clean_strict_end_separator($texte);
+            $texte = tool_string::clean_end_separator($texte);
+            $factory_mot->get_all_mots($texte);
+        }
+        return $factory_mot;
+    }
+
+}
+?>
---
+++ xhtml2vanilla.php
@@ -0,0 +1,46 @@
+<?php
+ mb_internal_encoding("utf-8");
+ require "./required/document/class_corpus.php";
+ require "./required/document/class_document.php";
+ require "./required/document/class_document_xml.php";
+
+ require "./required/files/tool_files_dirs.php";
+
+ require "./required/misc/class_tag.php";
+ require "./required/misc/tool_color.php";
+
+ require "./required/tree/class_tree.php";
+ require "./required/tree/class_tree_xhtml.php";
+ require "./required/tree/class_structure.php";
+ require "./required/tree/tool_tree.php";
+
+ require "./required/array/tool_array.php";
+
+ require "./required/maths/tool_maths.php";
+
+ require "./required/string/tool_string.php";
+ require "./required/string/class_segment.php";
+
+ require "./core.php";
+
+$path = './exemples/celex_IP-04-901.el.xml';
+$factory_mot = Core::path2fm($path);
+$rep = $factory_mot->__get_repartition_segment();
+
+$str_tok = "";
+foreach($rep as $id_doc => $doc){
+  foreach($doc as $array_segments){
+    foreach($array_segments as $id_segment){
+      $mot = $factory_mot->__get_word($id_segment);
+      $str_tok .= "$mot\n";
+    }
+    $str_tok .= ".EOS\n";
+  }
+}
+$str_tok .= ".EOP\n";
+
+$path_tok = $path.".tok";
+tool_files::file_write($path_tok,$str_tok);
+
+die();
+?>
